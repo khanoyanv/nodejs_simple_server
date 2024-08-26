ddca0f4fbc6df797c8a90962c532a20b9703f6c2
khanoyanv <113764269+khanoyanv@users.noreply.github.com>
2024-08-26 14:55:29 +0400

    connection to db

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
-//import express from "express"
-let express = require('express');
+// import express from 'express';
+async function connectToDb() {
+    await client.connect();
+}
+
+const express = require("express");
+const {MongoClient} = require('mongodb');
+const url = ('mongodb://localhost:27017');
+
 const app = express();
+const client = new MongoClient(url);
+
+connectToDb();
+let db = client.db('mydb');
+let users = db.collection('users');
+
+users.insertOne({name: 'Vahe', age: 22});
 
 app.listen(8081);
 
 app.get('/users', function (req, res) {
-	console.log(req.header);
-	res.send("You successfully got the list of users");
+    let user = findUser();
+    console.log(user);
+    res.send(user);
 });
 
-app.post('/posts', function (req, res) {
-	console.log(req.header);
-	res.send("You successfully got the list of posts");
-});
+function findUser() {
+    let user = users.findOne({name: "Vahe"});
+
+    return user;
+}
